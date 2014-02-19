4c4b8496d29ac07f31ce3f74cb43e4ef5f6317a2
Sheesh Mohsin <sheeshmohsin@gmail.com>
2014-02-19 20:10:11 +0530

    Added more packages

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='YourAppName',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe==0.18', 'bs4==4.3.1', 'urllib2', 'jinja2==2.7.1', 'Werkzeug==0.9.3'],
+      install_requires=['Flask>=0.7.2', 'MarkupSafe==0.18', 'bs4==4.3.1', 'urllib2', 'jinja2==2.7.1', 'Werkzeug==0.9.3', 'subprocess'],
      )
